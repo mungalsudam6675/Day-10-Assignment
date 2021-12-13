e84fa91623dfbef190c30ecea74a52082efeebb9
Mungal Sudam <MUngalsudam6675@gmail.com>
2021-12-13 14:07:50 +0530

    Adding UC-1

---
+++ UC-1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -x
+
+read -p"Enter first number : " num1
+read -p"Enter second number : " num2
+read -p"Enter thirrd number : " num3
+
+
+echo "Following are the user input number : $num1 $num2 $num3"
